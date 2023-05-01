382c64b71b7ef26efc2d395869cfd3e263c1767e
Patrick Kenny <patrickmkenny91@gmail.com>
2023-05-01 11:50:49 -0500

    tuple

---
+++ tuples.py
@@ -0,0 +1,12 @@
+# tuple = colelction which is ordered and unchangable used to gorup together related data
+
+student = ('Bro', 21, 'male')
+
+print(student.count('Bro'))
+print(student.index('male'))
+
+for x in student: 
+    print(x)
+
+if "Bro" in student: 
+    print("bro is here")
